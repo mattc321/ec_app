831694cc2ab50aeae651d4722cb07d1b2de5f1a4
Matt Campbell <mcampbell@ashlandfood.coop>
2017-05-30 15:18:34 +0000

    change in template markup testing new form array

--- ec_task_credits/theme/vocab-list.tpl.php
+++ ec_task_credits/theme/vocab-list.tpl.php
@@ -21,6 +21,17 @@
 <div class="credit-list-container">
     <?php foreach ($term_fields as $term_field): ?>
         <?php dpm($term_field); ?>
+        <div class="credit-row">
+            <div class="credit-cell credit-left">
+                <?php print $term_field['term_field']['#markup']; ?>
+            </div>
+            <div class="credit-cell credit-center">
+                
+            </div>
+            <div class="credit-cell credit-right">
+                <textarea name="credit-comment-<?php print $term_name->tid;?>" id="credit-comment-<?php print $term_name->tid;?>"></textarea>
+            </div>
+        </div>
     <?php endforeach; ?>
 
 
