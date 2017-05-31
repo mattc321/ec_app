f124668244e317fac782b6cd3f9a80dfa0b74ddc
Matt Campbell <mcampbell@ashlandfood.coop>
2017-05-31 16:10:35 +0000

    major form rework - roll back here

--- ec_task_credits/theme/vocab-list.tpl.php
+++ ec_task_credits/theme/vocab-list.tpl.php
@@ -1,40 +1,12 @@
-
+<?php
+/**
+* $terms - the raw vocabulary term array selected
+*   based on the type of task and title of the task
+* $term_fields - the form array containing the term
+*   and elements
+**/
+?>
 <h1>Vocab List</h1>
 <div class="credit-list-container">
-        <?php foreach ($terms as $term_name): ?>
-        <div class="credit-row">
-                <div class="credit-cell credit-left">
-                    <?php print $term_name->name; ?>
-                </div>
-                <div class="credit-cell credit-center">
-                    <button id="credit-browse-<?php print $term_name->tid;?>" class="btn btn-secondary">Browse</button>
-                </div>
-                <div class="credit-cell credit-right">
-                    <textarea name="credit-comment-<?php print $term_name->tid;?>" id="credit-comment-<?php print $term_name->tid;?>"></textarea>
-                </div>
-        </div>
-        <?php endforeach; ?>
-
-
+    <?php print render($test_form);?>
 </div>
-
-<div class="credit-list-container">
-    <?php foreach ($term_fields as $term_field): ?>
-        <?php dpm($term_field); ?>
-        <div class="credit-row">
-            <div class="credit-cell credit-left">
-                <?php print $term_field['term_field']['#markup']; ?>
-            </div>
-            <div class="credit-cell credit-center">
-                
-            </div>
-            <div class="credit-cell credit-right">
-                <textarea name="credit-comment-<?php print $term_name->tid;?>" id="credit-comment-<?php print $term_name->tid;?>"></textarea>
-            </div>
-        </div>
-    <?php endforeach; ?>
-
-
-</div>
-
-<?php dpm($term_fields);?>
