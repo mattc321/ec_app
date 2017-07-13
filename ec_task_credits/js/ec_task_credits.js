478b0d779afbce9a6b746abdf6a297ac9939f899
Matt Campbell <mcampbell@ashlandfood.coop>
2017-07-13 16:03:24 +0000

    added addtl function to units field collection to add multiple new blank rows at once

--- ec_task_credits/js/ec_task_credits.js
+++ ec_task_credits/js/ec_task_credits.js
@@ -25,7 +25,7 @@
 					$('.task-credit-complete').dequeue();
 				});
 
-				$('.task-credit-complete').delay(2000).queue(function(){
+				$('.task-credit-complete').delay(3500).queue(function(){
 					$('#ajax-credit-replace-' + $term_id).replaceWith('<div id="task-credit-ajax-response-' + $term_id + '" class="hide-field"></div>');
 					$('.task-credit-complete').dequeue();
 				});
@@ -39,11 +39,18 @@
 			var taskNid = $(this).data('id');
 			var myAjax = "/download/task-credits/" + taskNid;
 
-			$(this).delay(3500).queue(function(){
+			$(this).delay(2000).queue(function(){
 				$("#download-zip-target").load(myAjax);
+				$(this).dequeue();
+				$(this).html('Done Zipping');
 			});
+		});
 
-
+		$('#download-zip-target').click(function() {
+			$(this).addClass('animated zoomOutLeft').delay(2000).queue(function(){
+				$(this).replaceWith('<div id="download-zip-target"></div>');
+				$('#download-images-btn').html('Download Images');
+			});
 		});
 
 		if ($('.remove-image-js').length > 0) {
